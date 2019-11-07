4195a50c7782220bdabf6215cecb90228284cc08
srlane <scott.lane@ciholas.com>
2019-11-07 10:40:01 -0600

    Fixed bug in user and project models

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
   validates_uniqueness_of :title
   has_many :artifacts, dependent: :destroy
   has_many :user_projects
-  has_many :users, through :user_projects
+  has_many :users, through: :user_projects
   validate :free_plan_can_only_have_one_project
 
   def free_plan_can_only_have_one_project
